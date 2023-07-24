654f6926dd16a07c409d4bdd3cedf9ccb4424778
biitopher <biitopher@gmail.com>
2023-07-24 11:01:21 -0700

    [Fix] inseert sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,43 +3,35 @@
 /**
  * insertion_sort_list - Sorts doubly linked list in ascending order
  *
- * @list: Pointer to a pointer to the head of the list
+ * @list: Pointer to a pointer  of the list
  */
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	listint_t *current = (*list)->next;
 
-    listint_t *current = (*list)->next;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    while (current != NULL)
-    {
-        listint_t *temp = current->next;
-        listint_t *prev = current->prev;
+	while (current != NULL)
+	{
+		listint_t *temp = current->next;
+		listint_t *prev = current->prev;
 
-        while (prev != NULL && prev->n > current->n)
-        {
-            /* Swap the nodes */
-            if (prev->prev != NULL)
-                prev->prev->next = current;
-            else
-                *list = current;
-
-            if (current->next != NULL)
-                current->next->prev = prev;
-
-            prev->next = current->next;
-            current->prev = prev->prev;
-
-            prev->prev = current;
-            current->next = prev;
-
-            /* Print the list after swapping */
-            print_list(*list);
-
-            prev = current->prev;
-        }
-
-        current = temp;
-    }
+		while (prev != NULL && prev->n > current->n)
+		{
+			if (prev->prev != NULL)
+				prev->prev->next = current;
+			else
+				*list = current;
+			if (current->next != NULL)
+				current->next->prev = prev;
+			prev->next = current->next;
+			current->prev = prev->prev;
+			prev->prev = current;
+			current->next = prev;
+			print_list(*list);
+			prev = current->prev;
+		}
+		current = temp;
+	}
 }
