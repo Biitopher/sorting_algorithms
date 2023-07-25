0f83228ac49607793baf5e69d715f2016fc7b141
Evans Sese <sese_evans@yahoo.com>
2023-07-25 14:18:03 -0700

    [Feature] Task 6

---
+++ 102-counting_sort.c
@@ -0,0 +1,59 @@
+#include "sort.h"
+
+/**
+ * counting_sort - Sorts an array of integers in ascending order using Counting sort
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void counting_sort(int *array, size_t size)
+{
+	int max;
+	int *counting_array, *output_array, j;
+	size_t i;
+
+	if (array == NULL || size < 2)
+		return;
+	
+	max = array[0];
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	counting_array = malloc((max + 1) * sizeof(int));
+	if (counting_array == NULL)
+		return;
+
+	for (j = 0; j < max + 1; j++)
+		counting_array[j] = 0;
+
+	for (i = 0; i < size; i++)
+		counting_array[array[i]]++;
+
+	print_array(counting_array, max + 1);
+
+	for (j = 1; j < max + 1; j++)
+		counting_array[j] += counting_array[j - 1];
+
+	output_array = malloc(size * sizeof(int));
+	if (output_array == NULL)
+	{
+		free(counting_array);
+		return;
+	}
+
+	for (i = size - 1; i < size; i--)
+	{
+		output_array[counting_array[array[i]] - 1] = array[i];
+		counting_array[array[i]]--;
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = output_array[i];
+	print_array(array, size);
+
+	free(counting_array);
+	free(output_array);
+}
--- sort.h
+++ sort.h
@@ -30,5 +30,6 @@ void quick_sort_recursive(int *array, int low, int high, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
 void shell_sort(int *array, size_t size);
 size_t knuth_sequence(size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif
