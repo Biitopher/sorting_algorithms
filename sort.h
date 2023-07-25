e9c23388caba03af5c68caef9cefce0a40158745
biitopher <biitopher@gmail.com>
2023-07-25 15:31:56 -0700

    [Fix]Merge

--- sort.h
+++ sort.h
@@ -32,7 +32,7 @@ void shell_sort(int *array, size_t size);
 size_t knuth_sequence(size_t size);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void merge(int *left, size_t left_s, int *right, size_t right_s, int *array)
+void merge(int *left, size_t left_s, int *right, size_t right_s, int *array);
 
 
 #endif
