ddbcb8cc130f4155761cbe0a004f08f7157bc5e8
biitopher <biitopher@gmail.com>
2023-07-25 05:12:19 -0700

    [Fix] Betty

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -5,14 +5,14 @@
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
+
 void swap_ints(int *a, int *b)
 {
-    int tmp;
+	int tmp;
 
-
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 
@@ -27,26 +27,20 @@ void shell_sort(int *array, size_t size)
 {
 	size_t temp, i, j;
 
-
-    if (array == NULL || size < 2)
-        return;
-
-
-    for (temp = 1; temp < (size / 3);)
-        temp = temp * 3 + 1;
-
-
-    for (; temp >= 1; temp /= 3)
-    {
-        for (i = temp; i < size; i++)
-        {
-            j = i;
-            while (j >= temp && array[j - temp] > array[j])
+	if (array == NULL || size < 2)
+		return;
+	for (temp = 1; temp < (size / 3);)
+		temp = temp * 3 + 1;
+	for (; temp >= 1; temp /= 3)
+	{
+		for (i = temp; i < size; ++i)
+		{
+			j = i;
+			while (j >= temp && array[j - temp] > array[j])
 			{
 				swap_ints(array + j, array + (j - temp));
 				j -= temp;
 			}
-			print_array(array, size);
 		}
 	}
 }
