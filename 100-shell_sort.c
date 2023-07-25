9f6b3a94f8c04e5e11a9e87a055597bd42714034
biitopher <biitopher@gmail.com>
2023-07-25 07:56:00 -0700

    [Fix]Betty

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -2,19 +2,21 @@
 #include <stddef.h>
 
 /**
- * knuth_sequence - knuth_sequence
+ * knuth_sequence - knuth sequence
  * @size: data size
  *
+ * Return: size
  */
 
 size_t knuth_sequence(size_t size)
 {
-    size_t gap = 1;
-    while (gap < size)
-    {
-        gap = gap * 3 + 1;
-    }
-    return gap / 3;
+	size_t gap = 1;
+
+	while (gap < size)
+	{
+		gap = gap * 3 + 1;
+	}
+	return (gap / 3);
 }
 
 /**
@@ -26,24 +28,24 @@ size_t knuth_sequence(size_t size)
 
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = knuth_sequence(size);
-    size_t i;
+	size_t gap = knuth_sequence(size);
+	size_t i;
 
-    while (gap > 0) {
-        for (i = gap; i < size; ++i)
+	while (gap > 0)
 	{
-            int temp = array[i];
-            size_t j = i;
+		for (i = gap; i < size; ++i)
+		{
+			int temp = array[i];
+			size_t j = i;
 
-            while (j >= gap && array[j - gap] > temp)
-	    {
-                array[j] = array[j - gap];
-                j -= gap;
-            }
-
-            array[j] = temp;
-        }
-	print_array(array, size);
-	gap /=3;
-    }
+			while (j >= gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+			array[j] = temp;
+		}
+		print_array(array, size);
+		gap /= 3;
+	}
 }
