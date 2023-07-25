3e7304c22295e192e0fd5cacf1d77e4972432d62
Evans Sese <sese_evans@yahoo.com>
2023-07-25 09:03:49 -0700

    [Fix] Header file

--- sort.h
+++ sort.h
@@ -23,12 +23,12 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void swap_ints(int *x, int *y)
+void swap_ints(int *x, int *y);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quick_sort_recursive(int *array, int low, int high, size_t size);
 int lomuto_partition(int *array, int low, int high);
 void shell_sort(int *array, size_t size);
-size_t knuth_sequence(size_t size)
+size_t knuth_sequence(size_t size);
 
 #endif
